7be0a8acf8e19d22502f515b6da183164a2d38e9
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-26 15:59:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,18 +17,20 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
-  def self.reset_all
-    @@songs.clear
-  end
+  
+  extend Memorable
+  
+#  def self.reset_all
+#    @@songs.clear
+#  end
 
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  def self.count
-    self.all.count
-  end
+#  def self.count
+#    self.all.count
+#  end
 
   def self.all
     @@songs
