326c7fd5e3e3fff6ce57d2f6a26805693a8fabe6
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-26 16:25:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,28 +10,12 @@ class Song
     @@songs << self
   end
 
-#  def self.find_by_name(name)
-#    @@songs.detect{|a| a.name == name}
-#  end
-
   def artist=(artist)
     @artist = artist
   end
   
   extend Memorable, Findable
   include Paramable
-  
-#  def self.reset_all
-#    @@songs.clear
-#  end
-
-#  def to_param
-#    name.downcase.gsub(' ', '-')
-#  end
-
-#  def self.count
-#    self.all.count
-#  end
 
   def self.all
     @@songs
